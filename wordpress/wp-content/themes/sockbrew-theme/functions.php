4cf433e10da939867ccaa29f2c7d54078a79b61b
Olly <41718244+thesuperolly@users.noreply.github.com>
2021-09-27 12:06:16 +1000

    added uikit

--- wordpress/wp-content/themes/sockbrew-theme/functions.php
+++ wordpress/wp-content/themes/sockbrew-theme/functions.php
@@ -178,3 +178,14 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+add_action( 'wp_enqueue_scripts', 'custom_load_uikit' );
+/**
+ * Load UIKit.
+ */
+function custom_load_uikit() {
+    wp_enqueue_style( 'uikit', '//cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/css/uikit.min.css' );
+
+    wp_enqueue_script( 'uikit', '//cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit.min.js', array( 'jquery' ), '3.0.0.30', true );
+
+    wp_enqueue_script( 'uikit-icons', '//cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.30/js/uikit-icons.min.js', array( 'jquery' ), '3.0.0.30', true );
+}
