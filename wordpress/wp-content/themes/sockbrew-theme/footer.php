b4f53ec1584401747c28131cdebda8b7e2d32045
Kate McGinness <88216309+katemcginness@users.noreply.github.com>
2021-10-04 14:59:25 +1100

    footer customiser

--- wordpress/wp-content/themes/sockbrew-theme/footer.php
+++ wordpress/wp-content/themes/sockbrew-theme/footer.php
@@ -12,6 +12,57 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<h3>Location</h3>
+
+		<?php
+		// check to see if the logo exists and add it to the page
+		if ( get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_address' ) ) : ?>
+		
+		<p>
+			<?php echo get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_address' ); ?>
+		</p>
+		<?php // add a fallback if the logo doesn't exist
+		else : ?>
+		
+		<p>Building 1, 11 Kirinari St Bruce ACT 2617</p>
+		<?php endif; ?>
+		<h3>General Enquiries</h3>
+
+		<?php
+		// check to see if the logo exists and add it to the page
+		if ( get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_phone' ) ) : ?>
+		
+		<p>
+			Phone: <?php echo get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_phone' ); ?> (9am to 5pm)
+		</p>
+		<?php // add a fallback if the logo doesn't exist
+		else : ?>
+		
+		<p>+61 2 6201 5111</p>
+		<?php endif; ?>
+
+		<?php
+		// check to see if the logo exists and add it to the page
+		if ( get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_email' ) ) : ?>
+		
+		<p>
+			Email: <?php echo get_theme_mod( 'a_custom_theme_for_sockbrew_design_footer_email' ); ?>
+		</p>
+		<?php // add a fallback if the logo doesn't exist
+		else : ?>
+		
+		<p>contact@example.com</p>
+		<?php endif; ?>
+			<ul>
+				<li><a href="#">IBQC Leadership</a></li>
+				<li><a href="#">Publications</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		
+		
+		
+
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'a-custom-theme-for-sockbrew-design' ) ); ?>">
 				<?php
