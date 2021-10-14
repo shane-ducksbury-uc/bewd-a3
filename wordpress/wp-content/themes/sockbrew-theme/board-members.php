a103be18646e569633f4bcf701b773db0066bd2c
Olly <41718244+thesuperolly@users.noreply.github.com>
2021-10-15 01:21:39 +1100

    Lots of styles, formatted leadership cards

--- wordpress/wp-content/themes/sockbrew-theme/board-members.php
+++ wordpress/wp-content/themes/sockbrew-theme/board-members.php
@@ -12,85 +12,143 @@
 get_header();
 ?>
 
-	<main id="primary" class="forums-main">
+	<main id="primary" class="forums-main container">
 		<h1>IBQC Leadership</h1>
 
 		<div class="forums-container">
 
-			<?php
-		$board_member_posts_query = array(
-			'number-posts' => -1,
-			'post_type' => 'board-member'
-		);
-		
-		$board_members_posts = new WP_Query($board_member_posts_query);
-		
-		while ( $board_members_posts->have_posts() ) :
-			$board_members_posts->the_post(); ?>
-			<div class="uk-card uk-card-default">
-				<div class="uk-card-header">
-					<div class="uk-card-title">
-							<?php the_title(); ?></div>
-							<?php if(get_field('secondary_title')):
-								echo "<p>" . the_field('secondary_title') . "</p>";
-							endif;?>
-						</div>
-				<div class="uk-card-body">
-					<img src="<?php the_field('board_member_image'); ?>" />
-					<?php echo "<div>" . the_field('bio') . "</div>";
-					
-					if(get_field('professional_affiliations')): ?>
-						<table class="uk-table uk-table-striped">
-						<thead>
-							<th>Role</th>
-							<th>Professional Body</th>
-							<th>Year</th>
-						</thead>
-						<tbody>
-							<?php 		
-							$affiliations = get_field('professional_affiliations'); 
-							foreach ($affiliations as $affiliation): ?>
-								<tr>
-									<td><?php echo $affiliation['role']?></td>
-									<td><?php echo $affiliation['professional_body']?></td>
-									<td><?php echo $affiliation['pro_start_year'];
-									if($affiliation['pro_end_year']):
-										echo " - " . $affiliation['pro_end_year']; // This field is optional - hence the if	
-									endif;
-									?></td>
-								</tr>
-							<?php endforeach; ?>
-						</tbody>
-						</table>
-					<?php endif; ?>
-					
-					<?php if(get_field('awards')): ?>
-						<table class="uk-table uk-table-striped">
-						<thead>
-							<th>Award</th>
-							<th>Issued By</th>
-							<th>Year</th>
-						</thead>
-						<tbody>
-							<?php 		
-							$awards = get_field('awards'); 
-							foreach ($awards as $award): ?>
-								<tr>
-									<td><?php echo $award['award_title']?></td>
-									<td><?php echo $award['award_issuer']?></td>
-									<td><?php echo $award['award_year']; ?></td>
-								</tr>
-							<?php endforeach; ?>
-						</tbody>
-						</table>
-					<?php endif; ?>
-					
+		<!-- start leadership grid -->
+			<div class="leaderGrid">
+				<?php
+			$board_member_posts_query = array(
+				'number-posts' => -1,
+				'post_type' => 'board-member'
+			);
+			
+			$board_members_posts = new WP_Query($board_member_posts_query);
+			
+			while ( $board_members_posts->have_posts() ) :
+				$board_members_posts->the_post(); ?>
+				<div class="uk-card uk-card-default">
 
-				</div>
-				<div class="uk-card-footer">
-				</div>
-				</div>
-			<?php endwhile; ?>
+
+				<!-- card header start -->
+					<div class="uk-card-header">
+
+					<!-- olly's testing start -->
+
+					<div class="uk-grid-small uk-flex-top" uk-grid>
+						<div class="uk-width-auto">
+							<img class="uk-border-circle" src="<?php the_field('board_member_image'); ?>">
+						</div>
+						<div class="uk-width-expand">
+							<h3 class="uk-card-title uk-margin-remove-bottom"><?php the_title(); ?></h3>
+							
+						</div>
+
+					</div>
+
+					<!-- olly's testing end -->
+
+
+						
+							</div>
+							<!-- card header end -->
+							<!-- card body start -->
+					<div class="uk-card-body">
+
+						<?php if(get_field('secondary_title')): ?>
+							<p class="uk-text-meta uk-margin-remove-top"><?php the_field('secondary_title'); ?></p>
+						<?php endif;?>
+					
+						<?php if(get_field('bio')): ?>
+							<ul uk-accordion>
+								<li>
+									<a class="uk-accordion-title" href="#">Bio</a>
+									<div class="uk-accordion-content">
+									<?php echo "<p>" . the_field('bio') . "</p>"; ?>
+									</div>
+								</li>
+							</ul>
+						<?php endif;?>
+							
+
+					</div>
+					<!-- card body end -->
+
+
+					<!-- card footer start -->
+					<div class="uk-card-footer">
+
+						<ul uk-accordion>
+							<li>
+								<a class="uk-accordion-title" href="#">Positions</a>
+								<div class="uk-accordion-content">
+									
+								<?php if(get_field('professional_affiliations')): ?>
+									<?php 		
+										$affiliations = get_field('professional_affiliations'); 
+										foreach ($affiliations as $affiliation): ?>
+											<div class="professionalAttribution">
+												<h5>Type:</h5>
+												<p><?php echo $affiliation['role']?></p>
+												<h5>Body:</h5>
+												<p><?php echo $affiliation['professional_body']?></p>
+
+
+												<?php if($affiliation['pro_start_year']):?>
+													<h5>Year:</h5>
+													<div class="yearFrame">
+													<span><?php echo $affiliation['pro_start_year']; ?></span>
+														<?php if($affiliation['pro_end_year']==null){ ?>
+																</div>
+														<?php } ?>
+												<?php endif;?>
+												<?php if($affiliation['pro_end_year']):?>
+													<!-- // This field is optional - hence the if -->
+													<span> to <?php echo $affiliation['pro_end_year']; ?></span>
+													</div>
+												<?php endif;?>
+											</div>
+											
+										<?php endforeach; ?>
+								<?php endif; ?>
+
+								</div>
+							</li>
+							<li>
+								<a class="uk-accordion-title" href="#">Awards</a>
+								<div class="uk-accordion-content">
+									
+								<?php if(get_field('awards')): ?>
+									<?php 		
+										$awards = get_field('awards'); 
+										foreach ($awards as $award): ?>
+											<div class="professionalAttribution">
+												<h5>Award:</h5>
+												<p><?php echo $award['award_title']?></p>
+												<h5>Issuer:</h5>
+												<p><?php echo $award['award_issuer']?></p>
+												<h5>Year:</h5>
+												<p><?php echo $award['award_year']; ?></p>
+											</div>		
+									<?php endforeach; ?>
+								<?php endif; ?>
+
+								</div>
+							</li>
+						</ul>
+
+						
+
+						
+						
+
+					</div>
+					<!-- card footer end -->
+					</div>
+				<?php endwhile; ?>
+			</div>
 		</div>
 	</main>
 
