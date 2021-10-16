562e4fe853dd0b59829e59f7eefcc2fff41e9ba8
Olly <41718244+thesuperolly@users.noreply.github.com>
2021-10-16 18:08:20 +1100

    more styles mostly for single publication

--- wordpress/wp-content/themes/sockbrew-theme/single-publication.php
+++ wordpress/wp-content/themes/sockbrew-theme/single-publication.php
@@ -19,45 +19,56 @@ get_header();
 			
 			<div class="publication-info-container">
 				<!-- Could be refactored into a loop -->
-				<?php the_title('<h1>', '</h1>') ?>
-				<h3>Authors: <?php the_field('authors') ?></h3>
-				<p>Categories:</p>
-				<?php
-					$post_categories = get_field('category');
-					if ($post_categories) :
-						foreach ($post_categories as $post_category) : ?>
-							<span><?= $post_category->name ?></span>
-					<?php endforeach;
-					endif; ?>
-				<?php if(get_field('published_year')): ?>
-					<p>Year: <?= the_field('published_year');?></p>
-				<?php endif;?>
-				<p>Citation Details: <?= the_field('citation');?></p>
-				<p><?php
-					$title = get_the_title();
-				 	$authors = get_field('authors');
-					$year = get_field('published_year');
-					$url = get_field('link_to_publication');
+				<?php the_title('<h1 class="pub-title">', '</h1>') ?>
+				<div class="publication-header">
+					<div class="publication-details">
+						<h3>Authors: <?php the_field('authors') ?></h3>
 
-					if(!$year):
-						$year = "N.d";
-					endif;
-					
-					echo "${authors} (${year}) <em class='citation-em'>${title}</em> retrieved from ${url}";
+						<h3>Categories:</h3>
+						<?php
+							$post_categories = get_field('category');
+							if ($post_categories) :
+								foreach ($post_categories as $post_category) : ?>
+									<span class="uk-label"><?= $post_category->name ?></span>
+							<?php endforeach;
+							endif; ?>
+						<?php if(get_field('published_year')): ?>
+							<h3>Year: <?= the_field('published_year');?></h3>
+						<?php endif;?>
+						
+					</div>
+
+					<div class="publication-action-container">
+						<!-- Need to set a max width for this. -->
+						<img src="<?php the_field('publication_image')?>">
+						<a class="uk-button uk-button-primary" href=<?php the_field('link_to_publication')?>>View Publication</a>
+						<a class="uk-button uk-button-secondary" target="_blank" href="<?php echo generate_linkedin_url($_SERVER['REQUEST_URI'], get_the_title())?>">Share on LinkedIn</a>
+					</div>
+
+				</div>
+					<?php if(get_field('abstract')): ?>
+						<h3>Abstract:</h3> 
+						<p><?= the_field('abstract');?></p>
+					<?php endif;?>
+
+					<h3>Citation Details: <?= the_field('citation');?></h3>
+					<p class="citation-details"><?php
+						$title = get_the_title();
+						$authors = get_field('authors');
+						$year = get_field('published_year');
+						$url = get_field('link_to_publication');
+
+						if(!$year):
+							$year = "N.d";
+						endif;
+						
+						echo "${authors} (${year}) <em class='citation-em'>${title}</em> retrieved from ${url}";
 
 					?></p>
-				<?php if(get_field('abstract')): ?>
-					<p>Abstract:</p> 
-					<p><?= the_field('abstract');?></p>
-				<?php endif;?>
+				
 			</div>
 
-			<div class="publication-action-container">
-				<!-- Need to set a max width for this. -->
-				<img src="<?php the_field('publication_image')?>">
-				<a class="uk-button uk-button-primary" href=<?php the_field('link_to_publication')?>>View Publication</a>
-				<a class="uk-button uk-button-secondary" target="_blank" href="<?php echo generate_linkedin_url($_SERVER['REQUEST_URI'], get_the_title())?>">Share on LinkedIn</a>
-			</div>
+			
 			<?php endwhile; ?>
 	</main>
 <?php
